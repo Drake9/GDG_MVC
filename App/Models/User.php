b51abb4e3d3a7de212e9d081c2863e7ef9d65e29
Marcin Zapala <marcin.zapala.programista@gmail.com>
2019-09-04 20:22:05 +0200

    registration added

--- App/Models/User.php
+++ App/Models/User.php
@@ -9,18 +9,130 @@ use PDO;
  *
  * PHP version 7.0
  */
-class User extends \Core\Model
-{
+class User extends \Core\Model{
+	/**
+     * Error messages
+	 *
+	 *@var array
+     *
+     */
+    public $errors = [];
+	
+	/**
+     * Class constructor
+	 *
+	 *@param array $data  Initial property values
+     *
+     * @return void
+     */
+    public function __construct($data){
+        foreach($data as $key => $value){
+			$this->$key = $value;
+		}
+    }
 
     /**
-     * Get all the users as an associative array
+     * Save the user model with the current property values
      *
-     * @return array
+     * @return void
      */
-    public static function getAll()
-    {
-        $db = static::getDB();
-        $stmt = $db->query('SELECT id, name FROM users');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function save(){
+		
+		$this->validate();
+		
+		if(empty($this->errors)){
+			
+			$db = static::getDB();
+			
+			$hash = password_hash($this->password1, PASSWORD_DEFAULT);
+			
+			$sql = 'INSERT INTO users (login, email, password)
+				VALUES (:login, :email, :password)';
+				
+			$query = $db->prepare($sql);
+			
+			$query->bindValue(':login', $this->login, PDO::PARAM_STR);
+			$query->bindValue(':email', $this->email, PDO::PARAM_STR);
+			$query->bindValue(':password', $hash, PDO::PARAM_STR);
+			
+			return $query->execute();
+		}
+		
+		return false;
+    }
+	
+	/**
+     * Validate current property values, adding validation error messages to the errors array property
+     *
+     * @return void
+     */
+    public function validate(){
+		
+        /**
+		 * Login
+		 */
+		if ($this->login == ''){
+			$this->errors[] = "Login jest wymagany.";
+		}
+		if ((strlen($this->login) < 3) || (strlen($this->login) > 20)){
+			$this->errors[] = "Login musi posiadać od 3 do 20 znaków!";
+		}
+		if (ctype_alnum($this->login) == false){
+			$this->errors[] = "Login może składać się tylko z liter i cyfr (bez polskich znaków)";
+		}
+		
+		/**
+		 * Email
+		 */
+		$emailB = filter_var($this->email, FILTER_SANITIZE_EMAIL);
+		
+		if ((filter_var($emailB, FILTER_VALIDATE_EMAIL) == false) || ($emailB != $this->email)){
+			$this->errors[] = "Podaj poprawny adres e-mail!";
+		}
+		
+		if($this->emailExists($this->email)){
+			$this->errors[] = "Istnieje już konto przypisane do podanego adresu e-mail.";
+		}
+		
+		/**
+		 * Password
+		 */	
+		if ((strlen($this->password1) < 8) || (strlen($this->password1) > 30)){
+			$this->errors[] = "Hasło musi posiadać od 8 do 30 znaków!";
+		}
+		
+		if (preg_match('/.*[a-z]+.*/i', $this->password1) == 0){
+			$this->errors[] = "Hasło musi zawierać przynajmniej jedną literę.";
+		}
+		
+		if (preg_match('/.*\d+.*/i', $this->password1) == 0){
+			$this->errors[] = "Hasło musi zawierać przynajmniej jedną cyfrę.";
+		}
+		
+		if ($this->password1 != $this->password2){
+			$this->errors[] = "Podane hasła nie są identyczne!";
+		}	
+
+    }
+	
+	/**
+     * See if an email already exists in database
+	 *
+	 * @param string email address to search for
+     *
+     * @return boolean TRUE if exists, FALSE otherwise
+     */
+	protected function emailExists($email){	
+		$db = static::getDB();
+		
+		$sql = 'SELECT * FROM users WHERE email = :email';
+			
+		$query = $db->prepare($sql);
+		
+		$query->bindValue(':email', $email, PDO::PARAM_STR);
+		
+		$query->execute();
+		
+		return $query->fetch() !== false;
     }
 }
